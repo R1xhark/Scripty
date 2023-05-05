3dcde054314dbbe6c6996d70a1e9c069146fdbe5
Richard Dubný <72037908+R1xhark@users.noreply.github.com>
2023-05-05 14:42:32 +0200

    prepis poznamek
    Poznamky prelozeny z eng ---> cestiny

--- PXEbootcheck.py
+++ PXEbootcheck.py
@@ -5,26 +5,26 @@ import os
 import subprocess
 
 def check_pxe_boot():
-    # Check if the network interface is up
+    # Kontrola jestli je interface v poradku
     if "eth0" not in os.listdir("/sys/class/net"):
         print("Error>> eth0 not responding, check eth0 interface")
         return
 
-    # Check if PXE boot is enabled in BIOS
+    # Kontrola jestli je PXE boot povolen v biosu
     p = subprocess.Popen(["dmidecode", "-s", "bios-version"], stdout=subprocess.PIPE)
     output = p.communicate()[0].decode()
     if "PXE" not in output:
         print("Error>> PXE not active, please check bios settings")
         return
 
-    # Check if DHCP server is reachable
+    # Kontrola jestli je pristupny DHCP server
     p = subprocess.Popen(["ping", "-c", "1", "dhcp-server"], stdout=subprocess.PIPE)
     output = p.communicate()[0].decode()
     if "1 received" not in output:
         print("Error>> DHCP connection not reachable, please check DHCP setting or psysichal connection")
         return
 
-    # Check if TFTP server is reachable
+    # stejna kontrola jako u DHCP ale TFTP
     p = subprocess.Popen(["ping", "-c", "1", "tftp-server"], stdout=subprocess.PIPE)
     output = p.communicate()[0].decode()
     if "1 received" not in output:
